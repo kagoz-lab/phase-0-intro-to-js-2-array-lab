d841555c17dd333267630e29c8f6f03944368b7e
kagoz-lab <kagokevin33@gmail.com>
2024-11-25 00:26:34 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+
+  };
+
+
+  function destructivelyPrependCat() {
+    return cats.unshift("Bob");
+
+  };
+    
+  
+
+  function destructivelyRemoveLastCat() {
+    return cats.pop("Ralph");
+
+  }
+  function  destructivelyRemoveFirstCat() {
+    return cats.shift("Bob")
+}
+
+function appendCat() {
+    return cats.map((cat) => cat.concat("Broom"));
+
+}
+
+function prependCat() {
+    return ["Arnold"].concat(cats.map((cat) => cat));
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
+ 
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,12 @@
 require ( './helpers.js' );
 
+describe('Array functions', function () {
+  beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+  });
+
 describe('index.js', function () {
   describe('cats', function () {
     it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
@@ -7,12 +14,7 @@ describe('index.js', function () {
     });
   });
 
-  describe('Array functions', function () {
-    beforeEach(function () {
-      cats.length = 0;
-
-      cats.push('Milo', 'Otis', 'Garfield');
-    });
+ 
 
     describe('destructivelyAppendCat(name)', function () {
       it('appends a cat to the end of the cats array', function () {
